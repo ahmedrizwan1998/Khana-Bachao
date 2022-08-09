e7ba78c72865d76e7dc7ac6b9dcfe2c8c303c167
unknown <ahmedriz1998.ar@gmail.com>
2022-08-09 22:36:14 +0500

    init commit

---
+++ models/food.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    original_quantity: {
+        type: Number,
+        required: true,
+    },
+    source: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Organization'
+    },
+    current_quantity: {
+        type: Number,
+        required: true,
+        validate(value){
+            if(value < 0){
+                throw new Error("Current quantity can't be less than zero");
+            }
+        }
+    },
+    createdAt: {
+        type: Date,
+        expires: '12h',
+        default: Date().now
+    }
+},  {
+    timestamps: true,
+}
+);
+
+
+// checking
+foodSchema.pre('save', async function(next){
+    const food = this;
+    if(food.original_quantity < food.current_quantity){
+        throw new Error("Current quantity can't be greater than original quantity");
+    }
+    next();
+});
+
+
+const Food =  mongoose.model('Food', foodSchema);
+
+module.exports = Food;
